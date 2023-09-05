5142a13104e9e7d34dd73de694e1d2b556239bad
Denis Kosmachev <kosmadenis@gmail.com>
2023-09-06 02:42:36 +0300

    формат eject-кода

--- config/createEnvironmentHash.js
+++ config/createEnvironmentHash.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
-const { createHash } = require('crypto');
+const { createHash } = require('crypto')
 
-module.exports = env => {
-  const hash = createHash('md5');
-  hash.update(JSON.stringify(env));
+module.exports = (env) => {
+  const hash = createHash('md5')
+  hash.update(JSON.stringify(env))
 
-  return hash.digest('hex');
-};
+  return hash.digest('hex')
+}
